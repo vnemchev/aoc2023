a93cf64cfe8c1a078aa1abd5a10338949ed6b6de
vnemchev <vnemchev@gmail.com>
2023-12-02 16:39:47 +0100

    part 2 done

--- 02.js
+++ 02.js
@@ -6,12 +6,14 @@ const bag = {
 
 const solve = input => {
     const possibleGames = [];
+    let powerSum = 0;
     input.split('\n').forEach(row => {
         let validGame = true;
         const [game, rest] = row.split(': ');
         const gameId = Number(game.split(' ')[1]);
         const moves = rest.split('; ');
         const movesL = moves.length;
+        const fewestPossible = { red: 0, green: 0, blue: 0 };
 
         for (let i = 0; i < movesL; i++) {
             const move = moves[i];
@@ -24,13 +26,21 @@ const solve = input => {
                 const [countString, color] = cube.split(' ');
                 const count = Number(countString);
                 currentBag[color] -= count;
+                if (fewestPossible[color] < count)
+                    fewestPossible[color] = count;
             }
             if (Object.values(currentBag).some(v => v < 0)) validGame = false;
         }
         if (validGame) possibleGames.push(gameId);
+        powerSum += Object.values(fewestPossible).reduce(
+            (acc, curr) => acc * curr,
+        );
     });
 
-    return possibleGames.reduce((acc, curr) => acc + curr, 0);
+    return {
+        possibleSum: possibleGames.reduce((acc, curr) => acc + curr, 0),
+        powerSum,
+    };
 };
 
 console.log(
