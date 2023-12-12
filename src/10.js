02f96d539623be5e9df7b9204d3a24b1968deb0f
vnemchev <vnemchev@gmail.com>
2023-12-12 23:00:27 +0100

    name galaxies in universe

--- src/10.js
+++ src/10.js
@@ -1,13 +1,28 @@
+const HASHTAG = '#';
+const DOT = '.';
+
 function solve(input) {
-    const universe = expandUniverse(input);
+    const universe = nameGalaxies(expandUniverse(input));
+    // nameGalaxies(universe);
+    console.log(universe);
+}
+
+function nameGalaxies(universe) {
+    let galaxyNumber = 1;
+    const namedUniverse = [];
+    universe.forEach(row => {
+        while (row.some(e => e === HASHTAG)) {
+            row.splice(row.indexOf(HASHTAG), 1, galaxyNumber++);
+        }
+        namedUniverse.push(row);
+    });
+    return namedUniverse;
 }
 
 function expandUniverse(input) {
-    const universe = input.split('\n').map(r => r.split(''));
+    const universe = input.split('\n').map(row => row.split(''));
     const verticallyExpanded = expandColumns(universe);
-    const expandedUniverse = expandRows(verticallyExpanded);
-    const result = expandedUniverse.map(r => r.join(''));
-    return result.join('\n');
+    return expandRows(verticallyExpanded);
 }
 
 function expandColumns(input) {
@@ -15,11 +30,11 @@ function expandColumns(input) {
     for (let i = 0; i < universe[0].length; i++) {
         for (let j = 0; j < universe.length; j++) {
             const el = universe[j][i];
-            if (el !== '.') break;
+            if (el !== DOT) break;
             if (j !== universe.length - 1) continue;
             for (let k = 0; k < universe.length; k++) {
                 const row = universe[k];
-                row.splice(i, 0, '.');
+                row.splice(i, 0, DOT);
             }
             i++;
         }
@@ -31,7 +46,7 @@ function expandRows(input) {
     const universe = [...input];
     for (let i = 0; i < universe.length; i++) {
         const row = universe[i];
-        const rowHasGalaxy = row.some(e => e !== '.');
+        const rowHasGalaxy = row.some(e => e !== DOT);
         if (rowHasGalaxy) continue;
         universe.splice(i, 0, row);
         i++;
